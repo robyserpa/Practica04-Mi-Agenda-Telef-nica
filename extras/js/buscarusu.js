644acbf82a9853f4741185e5bbd4112cea775590
robyserpa <51723735+robyserpa@users.noreply.github.com>
2020-05-31 22:18:18 -0500

    Create buscarusu.js

---
+++ extras/js/buscarusu.js
@@ -0,0 +1,53 @@
+function buscarPorTexto() {
+    
+    var texto = document.getElementById("texto").value; 
+    if (texto == "") { 
+        document.getElementById("informacion").innerHTML = ""; 
+    } else { 
+        if (window.XMLHttpRequest) { 
+            // code for IE7+, Firefox, Chrome, Opera, Safari 
+            xmlhttp = new XMLHttpRequest(); 
+        } else { 
+            // code for IE6, IE5 
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
+        } 
+        xmlhttp.onreadystatechange = function() { 
+            if (this.readyState == 4 && this.status == 200) { 
+                //alert("llegue"); 
+                document.getElementById("informacion").innerHTML = this.responseText; 
+            } 
+        }; 
+    
+        xmlhttp.open("GET","../../../config/buscarusu.php?texto="+texto,true); 
+
+        xmlhttp.send(); 
+    } 
+    return false;
+}
+
+function buscarAnonimo() {
+    
+    var texto = document.getElementById("texto").value; 
+    if (texto == "") { 
+        document.getElementById("informacion").innerHTML = ""; 
+    } else { 
+        if (window.XMLHttpRequest) { 
+            // code for IE7+, Firefox, Chrome, Opera, Safari 
+            xmlhttp = new XMLHttpRequest(); 
+        } else { 
+            // code for IE6, IE5 
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
+        } 
+        xmlhttp.onreadystatechange = function() { 
+            if (this.readyState == 4 && this.status == 200) { 
+                //alert("llegue"); 
+                document.getElementById("informacion").innerHTML = this.responseText; 
+            } 
+        }; 
+    
+        xmlhttp.open("GET","../../config/buscarusu.php?texto="+texto,true); 
+
+        xmlhttp.send(); 
+    } 
+    return false;
+}
